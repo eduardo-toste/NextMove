359bc099c18279f805d2e255d232a2de915dc928
eduardo-toste <duciochetti@gmail.com>
2025-05-23 15:12:53 -0300

    feat: create stop all script

---
+++ stop-all.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "⛔ Parando todos os microserviços e containers..."
+
+# Para os containers
+cd infra/docker
+docker-compose down
+cd ../..
+
+# Para os processos Java (Spring Boot)
+pkill -f 'spring-boot:run'
+
+echo "✅ Tudo parado!"

04283554e86a96621bd92130e8b421c3b840da25
eduardo-toste <duciochetti@gmail.com>
2025-05-29 19:52:20 -0300

    feat: create user service

---
+++ services/auth-service/src/main/java/com/nextmove/auth_service/service/UserService.java
@@ -0,0 +1,29 @@
+package com.nextmove.auth_service.service;
+
+import com.nextmove.auth_service.dto.RegisterRequestDTO;
+import com.nextmove.auth_service.model.User;
+import com.nextmove.auth_service.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public void registerUser(RegisterRequestDTO request) {
+        var encodedPassword = passwordEncoder.encode(request.password());
+
+        var user = new User(
+                null,
+                request.name(),
+                request.username(),
+                encodedPassword
+        );
+
+        userRepository.save(user);
+    }
+}

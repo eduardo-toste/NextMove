0af00bc2a0090b46277e1e770f94c757032d7b5e
eduardo-toste <duciochetti@gmail.com>
2025-05-29 19:52:15 -0300

    feat: create jwt service

---
+++ services/auth-service/src/main/java/com/nextmove/auth_service/service/JwtService.java
@@ -0,0 +1,47 @@
+package com.nextmove.auth_service.service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class JwtService {
+
+    @Value("${api.security.token.secret}")
+    private String secret;
+    private static final long EXPIRATION = 1000 * 60 * 60;
+
+    private final Algorithm algorithm = Algorithm.HMAC256(secret);
+
+    public String generateToken(String username){
+        return JWT.create()
+                .withSubject(username)
+                .withIssuedAt(new Date())
+                .withExpiresAt(new Date(System.currentTimeMillis() + EXPIRATION))
+                .sign(algorithm);
+    }
+
+    public String extractUsername(String token){
+        return JWT.require(algorithm)
+                .build()
+                .verify(token)
+                .getSubject();
+    }
+
+    public boolean isTokenValid(String token, String username){
+        return extractUsername(token).equals(username) && !isExpired(token);
+    }
+
+    public boolean isExpired(String token){
+        Date expiresAt = JWT.require(algorithm)
+                .build()
+                .verify(token)
+                .getExpiresAt();
+
+        return expiresAt.before(new Date());
+    }
+
+}

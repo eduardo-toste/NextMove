26463d8f5c2302873430c01b82f2e42cee28f7c5
eduardo-toste <duciochetti@gmail.com>
2025-05-29 19:52:01 -0300

    fix: remove wrong field from auth request dto

--- services/auth-service/src/main/java/com/nextmove/auth_service/dto/AuthRequestDTO.java
+++ services/auth-service/src/main/java/com/nextmove/auth_service/dto/AuthRequestDTO.java
@@ -1,4 +1,4 @@
 package com.nextmove.auth_service.dto;
 
-public record AuthRequestDTO(String name, String username, String password) {
+public record AuthRequestDTO(String username, String password) {
 }

2d6d79f953f0e898ad92456df4630432b830858f
eduardo-toste <duciochetti@gmail.com>
2025-06-01 15:46:42 -0300

    refactor: change error builder to utility class

---
+++ services/auth-service/src/main/java/com/nextmove/auth_service/utils/ErrorBuilder.java
@@ -0,0 +1,21 @@
+package com.nextmove.auth_service.utils;
+
+import com.nextmove.auth_service.dto.ErrorResponseDTO;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@NoArgsConstructor
+public class ErrorBuilder {
+
+    public static ResponseEntity<ErrorResponseDTO> buildErrorResponse(HttpStatus status, String error, String message, String path) {
+        ErrorResponseDTO response = ErrorResponseDTO.of(
+                status.value(),
+                error,
+                message,
+                path
+        );
+        return ResponseEntity.status(status).body(response);
+    }
+
+}

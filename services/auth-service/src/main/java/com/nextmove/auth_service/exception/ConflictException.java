6b467b342bdc9ed6ab1e5450f82aff452f9f4d8d
eduardo-toste <duciochetti@gmail.com>
2025-06-01 15:30:30 -0300

    feat: create new custom exceptions

---
+++ services/auth-service/src/main/java/com/nextmove/auth_service/exception/ConflictException.java
@@ -0,0 +1,9 @@
+package com.nextmove.auth_service.exception;
+
+public class ConflictException extends RuntimeException {
+
+    public ConflictException(String message) {
+        super(message);
+    }
+
+}
---
+++ services/auth-service/src/main/java/com/nextmove/auth_service/exception/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.nextmove.auth_service.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+}

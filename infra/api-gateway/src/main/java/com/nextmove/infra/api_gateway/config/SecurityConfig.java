c387953c995e3d8758aa4e2d52b9fbdd487f0f2d
eduardo-toste <duciochetti@gmail.com>
2025-06-05 12:06:56 -0300

    feat: create gateway security config

---
+++ infra/api-gateway/src/main/java/com/nextmove/infra/api_gateway/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.nextmove.infra.api_gateway.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import javax.crypto.spec.SecretKeySpec;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Value("${api.security.token.secret}")
+    private String jwtSecret;
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeExchange(exchanges -> exchanges
+                        .pathMatchers("/auth-service/auth/**").permitAll()
+                        .anyExchange().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt.jwtDecoder(jwtDecoder()))
+                )
+                .build();
+    }
+
+    @Bean
+    public ReactiveJwtDecoder jwtDecoder() {
+        return NimbusReactiveJwtDecoder
+                .withSecretKey(new SecretKeySpec(jwtSecret.getBytes(), "HmacSHA256"))
+                .macAlgorithm(MacAlgorithm.HS256)
+                .build();
+    }
+}

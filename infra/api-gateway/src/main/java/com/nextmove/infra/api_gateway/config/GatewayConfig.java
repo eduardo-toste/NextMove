e374b3beaa1172f1ca605d4928f3e9b6f0dd45d2
eduardo-toste <duciochetti@gmail.com>
2025-06-05 12:06:51 -0300

    feat: create gateway general config

---
+++ infra/api-gateway/src/main/java/com/nextmove/infra/api_gateway/config/GatewayConfig.java
@@ -0,0 +1,20 @@
+package com.nextmove.infra.api_gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("auth-service", r -> r.path("/auth/**")
+                        .uri("lb://auth-service"))
+                .route("transaction-service", r -> r.path("/transactions/**")
+                        .uri("lb://transaction-service"))
+                .build();
+    }
+}

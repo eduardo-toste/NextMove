d9dc94bf1b8ccf9526c7154435816e929689d285
eduardo-toste <duciochetti@gmail.com>
2025-06-06 07:45:53 -0300

    fix: remove unused line in method

--- services/auth-service/src/main/java/com/nextmove/auth_service/security/SecurityConfiguration.java
+++ services/auth-service/src/main/java/com/nextmove/auth_service/security/SecurityConfiguration.java
@@ -5,7 +5,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -29,7 +28,6 @@ public class SecurityConfiguration {
                         .requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**").permitAll()
                         .anyRequest().authenticated()
                 )
-                .httpBasic(Customizer.withDefaults())
                 .build();
     }
 

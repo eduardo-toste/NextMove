9170b15a9f49e34a3cae1570dbd7b88f97c0d7e5
eduardo-toste <duciochetti@gmail.com>
2025-06-07 23:00:40 -0300

    feat: update transaction response dto

--- services/transaction-service/src/main/java/com/nextmove/transaction_service/dto/TransactionResponseDTO.java
+++ services/transaction-service/src/main/java/com/nextmove/transaction_service/dto/TransactionResponseDTO.java
@@ -1,4 +1,28 @@
 package com.nextmove.transaction_service.dto;
 
-public record TransactionResponseDTO() {
+import com.nextmove.transaction_service.model.Transaction;
+import com.nextmove.transaction_service.model.enums.TransactionType;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
+public record TransactionResponseDTO(
+
+        UUID id,
+        String title,
+        String description,
+        BigDecimal amount,
+        LocalDate createdAt,
+        LocalDate dueDate,
+        TransactionType type,
+        UUID userId
+
+) {
+
+    public TransactionResponseDTO(Transaction transaction){
+        this(transaction.getId(), transaction.getTitle(), transaction.getDescription(), transaction.getAmount(),
+                transaction.getCreatedAt(), transaction.getDueDate(), transaction.getType(), transaction.getUserId());
+    }
+
 }

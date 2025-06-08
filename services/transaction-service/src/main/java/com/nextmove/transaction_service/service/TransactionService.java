dc0d20915966b9fbaa25e66e00f1882ae2e514c1
eduardo-toste <duciochetti@gmail.com>
2025-06-08 14:57:41 -0300

    feat: create method to create transaction

--- services/transaction-service/src/main/java/com/nextmove/transaction_service/service/TransactionService.java
+++ services/transaction-service/src/main/java/com/nextmove/transaction_service/service/TransactionService.java
@@ -1,6 +1,8 @@
 package com.nextmove.transaction_service.service;
 
+import com.nextmove.transaction_service.dto.TransactionRequestDTO;
 import com.nextmove.transaction_service.dto.TransactionResponseDTO;
+import com.nextmove.transaction_service.exception.ResourceNotFoundException;
 import com.nextmove.transaction_service.model.Transaction;
 import com.nextmove.transaction_service.repository.TransactionRepository;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +10,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.UUID;
 
 @Service
@@ -16,12 +19,28 @@ public class TransactionService {
 
     private final TransactionRepository repository;
 
+    public TransactionResponseDTO createTransaction(UUID userId, TransactionRequestDTO request) {
+        Transaction transaction = new Transaction(
+                null,
+                request.title(),
+                request.description(),
+                request.amount(),
+                LocalDate.now(),
+                request.dueDate(),
+                request.type(),
+                userId
+        );
+
+        return new TransactionResponseDTO(repository.save(transaction));
+    }
+
     public Page<TransactionResponseDTO> getAllTransactions(UUID userId, Pageable pageable) {
         Page<Transaction> transactions = repository.findByUserId(userId, pageable);
 
-        // adicionar exception caso esteja vazio aqui
+        if (transactions.isEmpty()) {
+            throw new ResourceNotFoundException("You do not have transactions registered!");
+        }
 
         return transactions.map(TransactionResponseDTO::new);
     }
-
 }

1f185cd0646ef0dfa2701bca6269064a20341828
eduardo-toste <duciochetti@gmail.com>
2025-06-07 23:01:03 -0300

    feat: add method de find transactions by user id

--- services/transaction-service/src/main/java/com/nextmove/transaction_service/repository/TransactionRepository.java
+++ services/transaction-service/src/main/java/com/nextmove/transaction_service/repository/TransactionRepository.java
@@ -1,12 +1,14 @@
 package com.nextmove.transaction_service.repository;
 
 import com.nextmove.transaction_service.model.Transaction;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface TransactionRepository extends JpaRepository<Transaction, UUID> {
 
-
+    Page<Transaction> findByUserId(UUID userId, Pageable pageable);
 
 }

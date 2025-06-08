3053596f1bc449401d36f58c22aae65ececbbbbd
eduardo-toste <duciochetti@gmail.com>
2025-06-07 22:07:53 -0300

    feat: create transaction migration

---
+++ services/transaction-service/src/main/resources/db/migration/V1__create_transactions_table.sql
@@ -0,0 +1,12 @@
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+CREATE TABLE transactions (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    title VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    amount NUMERIC(19, 2) NOT NULL,
+    created_at DATE NOT NULL,
+    due_date DATE NOT NULL,
+    type VARCHAR(50) NOT NULL,
+    user_id UUID NOT NULL
+);

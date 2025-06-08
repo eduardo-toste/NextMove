d9f69490ccaf66ce480b74ef914c4d7899f756f9
eduardo-toste <duciochetti@gmail.com>
2025-06-07 22:34:18 -0300

    feat: add user id in token

--- services/auth-service/src/main/java/com/nextmove/auth_service/service/JwtService.java
+++ services/auth-service/src/main/java/com/nextmove/auth_service/service/JwtService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
+import java.util.UUID;
 
 @Service
 public class JwtService {
@@ -19,9 +20,10 @@ public class JwtService {
         return Algorithm.HMAC256(secret);
     }
 
-    public String generateToken(String username){
+    public String generateToken(String username, UUID id){
         return JWT.create()
                 .withSubject(username)
+                .withClaim("id", id.toString())
                 .withIssuedAt(new Date())
                 .withExpiresAt(new Date(System.currentTimeMillis() + EXPIRATION))
                 .sign(getAlgorithm());

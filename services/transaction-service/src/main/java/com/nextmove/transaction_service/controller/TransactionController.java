39c3166b5e6f7da314ad60845f02dba3745e87d4
eduardo-toste <duciochetti@gmail.com>
2025-06-07 23:01:37 -0300

    feat: add endpoint to get all transactions

--- services/transaction-service/src/main/java/com/nextmove/transaction_service/controller/TransactionController.java
+++ services/transaction-service/src/main/java/com/nextmove/transaction_service/controller/TransactionController.java
@@ -4,6 +4,7 @@ import com.nextmove.transaction_service.dto.TransactionResponseDTO;
 import com.nextmove.transaction_service.service.TransactionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.awt.print.Pageable;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/transaction")
@@ -21,8 +22,9 @@ public class TransactionController {
     private final TransactionService transactionService;
 
     @GetMapping
-    public ResponseEntity<Page<TransactionResponseDTO>> getAllTransactions(@RequestHeader("X-User-Name") String username, Pageable pageable) {
-        Page<TransactionResponseDTO> transactions =  transactionService.getAllTransactions(username, pageable);
+    public ResponseEntity<Page<TransactionResponseDTO>> getAllTransactions(@RequestHeader("X-User-Id") String userIdHeader, Pageable pageable) {
+        UUID userId = UUID.fromString(userIdHeader);
+        Page<TransactionResponseDTO> transactions =  transactionService.getAllTransactions(userId, pageable);
 
         return ResponseEntity.status(HttpStatus.OK).body(transactions);
     }

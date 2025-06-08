7ec5f5288913605d32157b90d6b36aaefeff0068
eduardo-toste <duciochetti@gmail.com>
2025-06-07 23:01:25 -0300

    feat: add method to get all transactions

--- services/transaction-service/src/main/java/com/nextmove/transaction_service/service/TransactionService.java
+++ services/transaction-service/src/main/java/com/nextmove/transaction_service/service/TransactionService.java
@@ -1,16 +1,27 @@
 package com.nextmove.transaction_service.service;
 
 import com.nextmove.transaction_service.dto.TransactionResponseDTO;
+import com.nextmove.transaction_service.model.Transaction;
+import com.nextmove.transaction_service.repository.TransactionRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.awt.print.Pageable;
+import java.util.UUID;
 
 @Service
+@RequiredArgsConstructor
 public class TransactionService {
 
-    public Page<TransactionResponseDTO> getAllTransactions(String username, Pageable pageable) {
-        return null;
+    private final TransactionRepository repository;
+
+    public Page<TransactionResponseDTO> getAllTransactions(UUID userId, Pageable pageable) {
+        Page<Transaction> transactions = repository.findByUserId(userId, pageable);
+
+        // adicionar exception caso esteja vazio aqui
+
+        return transactions.map(TransactionResponseDTO::new);
     }
 
 }

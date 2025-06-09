6699f8fedee6d71876ed180766f0ff1eb93e218b
eduardo-toste <duciochetti@gmail.com>
2025-06-09 19:32:00 -0300

    feat: create transaction put request dto

---
+++ services/transaction-service/src/main/java/com/nextmove/transaction_service/dto/TransactionPutRequestDTO.java
@@ -0,0 +1,24 @@
+package com.nextmove.transaction_service.dto;
+
+import com.nextmove.transaction_service.model.enums.Status;
+import com.nextmove.transaction_service.model.enums.TransactionType;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public record TransactionPutRequestDTO(
+
+        @NotBlank String title,
+        @NotBlank String description,
+        @NotNull @Positive BigDecimal amount,
+        @NotNull @Future LocalDate dueDate,
+        @NotNull TransactionType type,
+        @NotNull Status status
+
+) {
+
+}

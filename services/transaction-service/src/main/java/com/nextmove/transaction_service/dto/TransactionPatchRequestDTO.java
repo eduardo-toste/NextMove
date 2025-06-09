c202be63d3fdc949b009adcc2a515e8d16ff7da8
eduardo-toste <duciochetti@gmail.com>
2025-06-09 07:53:35 -0300

    feat: create patch request dto

---
+++ services/transaction-service/src/main/java/com/nextmove/transaction_service/dto/TransactionPatchRequestDTO.java
@@ -0,0 +1,20 @@
+package com.nextmove.transaction_service.dto;
+
+import com.nextmove.transaction_service.model.enums.TransactionType;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.Positive;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public record TransactionPatchRequestDTO(
+
+        String title,
+        String description,
+        @Positive BigDecimal amount,
+        @Future LocalDate dueDate,
+        TransactionType type
+
+) {
+
+}

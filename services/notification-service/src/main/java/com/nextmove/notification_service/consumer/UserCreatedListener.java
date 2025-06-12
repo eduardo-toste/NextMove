263e4d53051b499d61504ac744697f451e107865
eduardo-toste <duciochetti@gmail.com>
2025-06-12 14:10:34 -0300

    feat: create user created listener

---
+++ services/notification-service/src/main/java/com/nextmove/notification_service/consumer/UserCreatedListener.java
@@ -0,0 +1,20 @@
+package com.nextmove.notification_service.consumer;
+
+import com.nextmove.notification_service.config.RabbitMQConfig;
+import com.nextmove.notification_service.dto.UserCreatedEvent;
+import com.nextmove.notification_service.service.EmailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class UserCreatedListener {
+
+    private final EmailService emailService;
+
+    @RabbitListener(queues = RabbitMQConfig.USER_CREATED_QUEUE)
+    public void onUserCreated(UserCreatedEvent event) {
+        emailService.sendWelcomeEmail(event.email(), event.nome());
+    }
+}

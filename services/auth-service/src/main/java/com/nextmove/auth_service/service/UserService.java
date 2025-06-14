bfe47a62239d11714c04fc23bc09a5e91a86e1e1
eduardo-toste <duciochetti@gmail.com>
2025-06-13 21:28:59 -0300

    feat: send welcome message with user service

--- services/auth-service/src/main/java/com/nextmove/auth_service/service/UserService.java
+++ services/auth-service/src/main/java/com/nextmove/auth_service/service/UserService.java
@@ -1,8 +1,10 @@
 package com.nextmove.auth_service.service;
 
 import com.nextmove.auth_service.dto.RegisterRequestDTO;
+import com.nextmove.auth_service.dto.UserCreatedEvent;
 import com.nextmove.auth_service.exception.ExistentUserException;
 import com.nextmove.auth_service.model.User;
+import com.nextmove.auth_service.producer.UserEventProducer;
 import com.nextmove.auth_service.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -14,6 +16,7 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final UserEventProducer userEventProducer;
 
     public void registerUser(RegisterRequestDTO request) {
         var searchedUser = userRepository.findByUsername(request.username());
@@ -30,6 +33,9 @@ public class UserService {
                 request.username(),
                 encodedPassword
         );
+
+        UserCreatedEvent event = new UserCreatedEvent(user.getName(), user.getUsername());
+        userEventProducer.sendUserCreatedEvent(event);
 
         userRepository.save(user);
     }

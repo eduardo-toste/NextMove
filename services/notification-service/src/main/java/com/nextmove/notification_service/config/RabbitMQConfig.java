e4d887c8c0a58455488adff70d1f58bd53557b0a
eduardo-toste <duciochetti@gmail.com>
2025-06-14 10:23:53 -0300

    fix: add jackson converter to listener config

--- services/notification-service/src/main/java/com/nextmove/notification_service/config/RabbitMQConfig.java
+++ services/notification-service/src/main/java/com/nextmove/notification_service/config/RabbitMQConfig.java
@@ -4,6 +4,7 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -27,5 +28,10 @@ public class RabbitMQConfig {
     @Bean
     public Binding userCreatedBinding(Queue userCreatedQueue, DirectExchange directExchange) {
         return BindingBuilder.bind(userCreatedQueue).to(directExchange).with(USER_CREATED_ROUTING_KEY);
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
     }
 }

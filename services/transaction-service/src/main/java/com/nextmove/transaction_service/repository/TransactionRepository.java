5afb02864da17e068d2924935ee8e69f1882ef9a
eduardo-toste <duciochetti@gmail.com>
2025-06-14 18:18:45 -0300

    feat: create find by due date method

--- services/transaction-service/src/main/java/com/nextmove/transaction_service/repository/TransactionRepository.java
+++ services/transaction-service/src/main/java/com/nextmove/transaction_service/repository/TransactionRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 
 public interface TransactionRepository extends JpaRepository<Transaction, UUID> {
@@ -12,5 +14,7 @@ public interface TransactionRepository extends JpaRepository<Transaction, UUID> 
     Page<Transaction> findByUserId(UUID userId, Pageable pageable);
 
     Transaction findByUserIdAndId(UUID userId, UUID transactionId);
+
+    List<Transaction> findByDueDate(LocalDate dueDate);
 
 }

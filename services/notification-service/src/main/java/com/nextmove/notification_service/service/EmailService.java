37bff02028bf25aa10bdf00e312f1ca9126acd43
eduardo-toste <duciochetti@gmail.com>
2025-06-17 16:19:35 -0300

    feat: create send transaction reminder mail method

--- services/notification-service/src/main/java/com/nextmove/notification_service/service/EmailService.java
+++ services/notification-service/src/main/java/com/nextmove/notification_service/service/EmailService.java
@@ -1,6 +1,7 @@
 package com.nextmove.notification_service.service;
 
 import com.nextmove.notification_service.config.GetEnv;
+import com.nextmove.notification_service.dto.TransactionReminderEvent;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
@@ -37,6 +38,32 @@ public class EmailService {
             mailSender.send(message);
         } catch (MessagingException e) {
             throw new RuntimeException("Send e-mail error", e);
+        }
+    }
+
+    public void sendTransactionReminderEmail(TransactionReminderEvent event) {
+        Context context = new Context();
+        context.setVariable("nome", event.name());
+        context.setVariable("title", event.title());
+        context.setVariable("description", event.description());
+        context.setVariable("dueDate", event.dueDate().toString());
+        context.setVariable("amount", "R$ " + event.amount().setScale(2).toString().replace(".", ","));
+        context.setVariable("type", event.type().equalsIgnoreCase("INCOME") ? "Receita" : "Despesa");
+
+        String htmlContent = templateEngine.process("transaction-reminder", context);
+
+        try {
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+
+            helper.setTo(event.email());
+            helper.setSubject("🔔 Lembrete de Transação no NextMove");
+            helper.setFrom(from);
+            helper.setText(htmlContent, true);
+
+            mailSender.send(message);
+        } catch (MessagingException e) {
+            throw new RuntimeException("Erro ao enviar e-mail de lembrete", e);
         }
     }
 }

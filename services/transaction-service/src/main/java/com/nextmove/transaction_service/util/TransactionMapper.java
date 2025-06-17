ebadd64e04c1c1b0875096d6782af59e93ee78f4
eduardo-toste <duciochetti@gmail.com>
2025-06-17 15:17:54 -0300

    fix: convert type do string

--- services/transaction-service/src/main/java/com/nextmove/transaction_service/util/TransactionMapper.java
+++ services/transaction-service/src/main/java/com/nextmove/transaction_service/util/TransactionMapper.java
@@ -43,7 +43,7 @@ public class TransactionMapper {
                 transaction.getDescription(),
                 transaction.getDueDate(),
                 transaction.getAmount(),
-                transaction.getType()
+                transaction.getType().toString()
         );
     }
 

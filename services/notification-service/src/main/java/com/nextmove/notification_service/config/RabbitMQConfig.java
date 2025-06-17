febee146e812c07891bf5625effac02863c38ef7
eduardo-toste <duciochetti@gmail.com>
2025-06-17 16:19:11 -0300

    feat: create queue and binding for transaction reminder

--- services/notification-service/src/main/java/com/nextmove/notification_service/config/RabbitMQConfig.java
+++ services/notification-service/src/main/java/com/nextmove/notification_service/config/RabbitMQConfig.java
@@ -13,11 +13,18 @@ public class RabbitMQConfig {
 
     public static final String USER_CREATED_QUEUE = "user.created.queue";
     public static final String USER_CREATED_ROUTING_KEY = "user.created";
+    public static final String TRANSACTION_REMINDER_QUEUE = "transaction.reminder.queue";
+    public static final String TRANSACTION_REMINDER_ROUTING_KEY = "transaction.reminder";
     public static final String EXCHANGE = "nextmove.direct";
 
     @Bean
     public Queue userCreatedQueue() {
         return new Queue(USER_CREATED_QUEUE, true);
+    }
+
+    @Bean
+    public Queue transactionReminderQueue() {
+        return new Queue(TRANSACTION_REMINDER_QUEUE);
     }
 
     @Bean
@@ -28,6 +35,11 @@ public class RabbitMQConfig {
     @Bean
     public Binding userCreatedBinding(Queue userCreatedQueue, DirectExchange directExchange) {
         return BindingBuilder.bind(userCreatedQueue).to(directExchange).with(USER_CREATED_ROUTING_KEY);
+    }
+
+    @Bean
+    public Binding transactionReminderBinding(Queue transactionReminderQueue, DirectExchange directExchange) {
+        return BindingBuilder.bind(transactionReminderQueue).to(directExchange).with(TRANSACTION_REMINDER_ROUTING_KEY);
     }
 
     @Bean

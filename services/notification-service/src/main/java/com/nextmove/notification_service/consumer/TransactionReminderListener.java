cd02dd1a329dc4d543800cf8ca20d359bc31e534
eduardo-toste <duciochetti@gmail.com>
2025-06-19 16:16:33 -0300

    feat: implementing retries and error handler at transaction listener

--- services/notification-service/src/main/java/com/nextmove/notification_service/consumer/TransactionReminderListener.java
+++ services/notification-service/src/main/java/com/nextmove/notification_service/consumer/TransactionReminderListener.java
@@ -3,8 +3,10 @@ package com.nextmove.notification_service.consumer;
 import com.nextmove.notification_service.config.RabbitMQConfig;
 import com.nextmove.notification_service.dto.TransactionReminderEvent;
 import com.nextmove.notification_service.service.EmailService;
+import com.nextmove.notification_service.util.RabbitRetryHandler;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
@@ -14,18 +16,28 @@ import org.springframework.stereotype.Component;
 public class TransactionReminderListener {
 
     private final EmailService emailService;
+    private final RabbitRetryHandler retryHandler;
 
     @RabbitListener(queues = RabbitMQConfig.TRANSACTION_REMINDER_QUEUE)
-    public void onTransactionReminder(TransactionReminderEvent event) {
-        log.info("📩 Recebido Transaction Reminder: {}", event);
+    public void onTransactionReminder(TransactionReminderEvent event, Message message) {
+        int retryCount = retryHandler.getRetryCount(message, RabbitMQConfig.TRANSACTION_REMINDER_QUEUE);
+
+        log.info("📩 [TransactionReminder] Recebido: {} | Tentativa: {}", event, retryCount + 1);
 
         try {
             emailService.sendTransactionReminderEmail(event);
-            log.info("✅ Email de lembrete enviado para: {}", event.email());
+            log.info("✅ [TransactionReminder] Email enviado para: {}", event.email());
 
         } catch (Exception ex) {
-            log.error("❌ Erro ao processar Transaction Reminder para {}: {}", event.email(), ex.getMessage(), ex);
-            throw ex;
+            retryHandler.handleError(
+                    event,
+                    ex,
+                    retryCount,
+                    RabbitMQConfig.DLQ_EXCHANGE,
+                    RabbitMQConfig.TRANSACTION_REMINDER_DLQ_ROUTING_KEY,
+                    "TransactionReminder",
+                    event.email()
+            );
         }
     }
 }

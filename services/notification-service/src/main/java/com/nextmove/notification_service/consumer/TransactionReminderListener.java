c1cd146103a99c7d25bc0a0d6cb18a7fdc875610
eduardo-toste <duciochetti@gmail.com>
2025-06-19 15:38:19 -0300

    feat: implement try catch at transaction reminder listener

--- services/notification-service/src/main/java/com/nextmove/notification_service/consumer/TransactionReminderListener.java
+++ services/notification-service/src/main/java/com/nextmove/notification_service/consumer/TransactionReminderListener.java
@@ -4,9 +4,11 @@ import com.nextmove.notification_service.config.RabbitMQConfig;
 import com.nextmove.notification_service.dto.TransactionReminderEvent;
 import com.nextmove.notification_service.service.EmailService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class TransactionReminderListener {
@@ -15,7 +17,15 @@ public class TransactionReminderListener {
 
     @RabbitListener(queues = RabbitMQConfig.TRANSACTION_REMINDER_QUEUE)
     public void onTransactionReminder(TransactionReminderEvent event) {
-        emailService.sendTransactionReminderEmail(event);
-    }
+        log.info("📩 Recebido Transaction Reminder: {}", event);
 
+        try {
+            emailService.sendTransactionReminderEmail(event);
+            log.info("✅ Email de lembrete enviado para: {}", event.email());
+
+        } catch (Exception ex) {
+            log.error("❌ Erro ao processar Transaction Reminder para {}: {}", event.email(), ex.getMessage(), ex);
+            throw ex;
+        }
+    }
 }

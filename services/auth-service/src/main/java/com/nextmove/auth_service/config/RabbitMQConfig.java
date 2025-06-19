73ca8cc1f6e6374df6a2c5c0a765dad9b404dd3f
eduardo-toste <duciochetti@gmail.com>
2025-06-19 11:31:15 -0300

    refactor: update rabbitmq config at auth service

--- services/auth-service/src/main/java/com/nextmove/auth_service/config/RabbitMQConfig.java
+++ services/auth-service/src/main/java/com/nextmove/auth_service/config/RabbitMQConfig.java
@@ -1,37 +1,22 @@
 package com.nextmove.auth_service.config;
 
 import org.springframework.amqp.core.DirectExchange;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
 
-    public static final String USER_CREATED_QUEUE = "user.created.queue";
-    public static final String USER_CREATED_ROUTING_KEY = "user.created";
     public static final String EXCHANGE = "nextmove.direct";
+
+    public static final String USER_CREATED_ROUTING_KEY = "user.created";
 
     @Bean
     public DirectExchange directExchange() {
         return new DirectExchange(EXCHANGE);
-    }
-
-    @Bean
-    public Queue userCreatedQueue() {
-        return new Queue(USER_CREATED_QUEUE, true);
-    }
-
-    @Bean
-    public Binding userCreatedBinding(Queue userCreatedQueue, DirectExchange directExchange) {
-        return BindingBuilder.bind(userCreatedQueue)
-                .to(directExchange)
-                .with(USER_CREATED_ROUTING_KEY);
     }
 
     @Bean

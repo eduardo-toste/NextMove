c5bf75a06aae4bbd6f14348c2a83a5eb51f78751
eduardo-toste <duciochetti@gmail.com>
2025-06-19 11:31:29 -0300

    refactor: update rabbitmq config at transaction service

--- services/transaction-service/src/main/java/com/nextmove/transaction_service/config/RabbitMQConfig.java
+++ services/transaction-service/src/main/java/com/nextmove/transaction_service/config/RabbitMQConfig.java
@@ -1,9 +1,6 @@
 package com.nextmove.transaction_service.config;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
-import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -13,25 +10,12 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RabbitMQConfig {
 
-    public static final String TRANSACTION_REMINDER_QUEUE = "transaction.reminder.queue";
-    public static final String TRANSACTION_REMINDER_ROUTING_KEY = "transaction.reminder";
     public static final String EXCHANGE = "nextmove.direct";
+    public static final String TRANSACTION_REMINDER_ROUTING_KEY = "transaction.reminder";
 
     @Bean
     public DirectExchange directExchange() {
         return new DirectExchange(EXCHANGE);
-    }
-
-    @Bean
-    public Queue transactionReminderQueue() {
-        return new Queue(TRANSACTION_REMINDER_QUEUE);
-    }
-
-    @Bean
-    public Binding transactionRemindeBinding(Queue transactionReminderQueue, DirectExchange directExchange) {
-        return BindingBuilder.bind(transactionReminderQueue)
-                .to(directExchange)
-                .with(TRANSACTION_REMINDER_ROUTING_KEY);
     }
 
     @Bean
@@ -46,5 +30,4 @@ public class RabbitMQConfig {
         template.setMessageConverter(messageConverter);
         return template;
     }
-
 }

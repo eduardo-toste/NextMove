53629a55a384abfb5a7f82eb0528526f20e2c02b
eduardo-toste <duciochetti@gmail.com>
2025-06-19 15:38:26 -0300

    feat: implement try catch at user created listener

--- services/notification-service/src/main/java/com/nextmove/notification_service/consumer/UserCreatedListener.java
+++ services/notification-service/src/main/java/com/nextmove/notification_service/consumer/UserCreatedListener.java
@@ -4,9 +4,11 @@ import com.nextmove.notification_service.config.RabbitMQConfig;
 import com.nextmove.notification_service.dto.UserCreatedEvent;
 import com.nextmove.notification_service.service.EmailService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class UserCreatedListener {
@@ -15,6 +17,15 @@ public class UserCreatedListener {
 
     @RabbitListener(queues = RabbitMQConfig.USER_CREATED_QUEUE)
     public void onUserCreated(UserCreatedEvent event) {
-        emailService.sendWelcomeEmail(event.email(), event.name());
+        log.info("📩 Recebido User Created Event: {}", event);
+
+        try {
+            emailService.sendWelcomeEmail(event.email(), event.name());
+            log.info("✅ Email de boas-vindas enviado para: {}", event.email());
+
+        } catch (Exception ex) {
+            log.error("❌ Erro ao enviar email de boas-vindas para {}: {}", event.email(), ex.getMessage(), ex);
+            throw ex;
+        }
     }
 }

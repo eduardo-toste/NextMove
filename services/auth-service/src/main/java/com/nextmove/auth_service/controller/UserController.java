859ba99525ef831d79c60aefe78d76e4ac31c515
eduardo-toste <duciochetti@gmail.com>
2025-06-21 17:30:31 -0300

    refactor: implement better practices and documentation in users controller

--- services/auth-service/src/main/java/com/nextmove/auth_service/controller/UserController.java
+++ services/auth-service/src/main/java/com/nextmove/auth_service/controller/UserController.java
@@ -2,8 +2,12 @@ package com.nextmove.auth_service.controller;
 
 import com.nextmove.auth_service.dto.UserResponseDTO;
 import com.nextmove.auth_service.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
 
+@Tag(name = "User", description = "Endpoints for user management")
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
@@ -19,11 +24,20 @@ public class UserController {
 
     private final UserService userService;
 
+    @Operation(
+            summary = "Get user by ID",
+            description = "Retrieve user details by their unique ID",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "User found",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = UserResponseDTO.class))),
+                    @ApiResponse(responseCode = "404", description = "User not found",
+                            content = @Content)
+            }
+    )
     @GetMapping("/{userId}")
     public ResponseEntity<UserResponseDTO> getUserById(@PathVariable UUID userId) {
         UserResponseDTO user = userService.getUserById(userId);
-
-        return ResponseEntity.status(HttpStatus.OK).body(user);
+        return ResponseEntity.ok(user);
     }
-
 }

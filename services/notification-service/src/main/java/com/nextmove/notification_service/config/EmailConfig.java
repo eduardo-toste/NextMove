6be867c1c35c63aa7721ca47bd153111bb93df5b
eduardo-toste <duciochetti@gmail.com>
2025-06-23 06:53:13 -0300

    fix: update values from email settings

--- services/notification-service/src/main/java/com/nextmove/notification_service/config/EmailConfig.java
+++ services/notification-service/src/main/java/com/nextmove/notification_service/config/EmailConfig.java
@@ -1,5 +1,6 @@
 package com.nextmove.notification_service.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -10,17 +11,32 @@ import java.util.Properties;
 @Configuration
 public class EmailConfig {
 
+    @Value("${mail.host}")
+    private String host;
+
+    @Value("${mail.port}")
+    private int port;
+
+    @Value("${mail.username}")
+    private String username;
+
+    @Value("${mail.password}")
+    private String password;
+
+    @Value("${mail.tls-enabled}")
+    private boolean tlsEnabled;
+
     @Bean
     public JavaMailSender mailSender() {
         JavaMailSenderImpl sender = new JavaMailSenderImpl();
-        sender.setHost(GetEnv.get("MAIL_HOST"));
-        sender.setPort(Integer.parseInt(GetEnv.get("MAIL_PORT")));
-        sender.setUsername(GetEnv.get("MAIL_USERNAME"));
-        sender.setPassword(GetEnv.get("MAIL_PASSWORD"));
+        sender.setHost(host);
+        sender.setPort(port);
+        sender.setUsername(username);
+        sender.setPassword(password);
 
         Properties props = sender.getJavaMailProperties();
         props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", GetEnv.get("MAIL_TLS_ENABLED"));
+        props.put("mail.smtp.starttls.enable", String.valueOf(tlsEnabled));
 
         return sender;
     }

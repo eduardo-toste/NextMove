049bc2da541c8da1fb29c510267ba329acedb308
eduardo-toste <duciochetti@gmail.com>
2025-06-23 16:00:17 -0300

    doc: create open api config for auth service

---
+++ services/auth-service/src/main/java/com/nextmove/auth_service/config/OpenApiConfig.java
@@ -0,0 +1,28 @@
+package com.nextmove.auth_service.config;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI nextMoveAuthOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Auth Service API - NextMove")
+                        .description("API responsável pela autenticação e registro de usuários no NextMove.")
+                        .version("v1.0")
+                        .contact(new Contact()
+                                .name("Equipe NextMove")
+                                .email("contato@nextmove.com"))
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Documentação completa NextMove")
+                        .url("https://nextmove.com/docs"));
+    }
+}

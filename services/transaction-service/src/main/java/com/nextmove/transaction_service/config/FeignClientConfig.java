55bf9c5baa9c31e6f09fdad12bb972bf304f707a
eduardo-toste <duciochetti@gmail.com>
2025-06-23 16:13:51 -0300

    fix: update service token value

--- services/transaction-service/src/main/java/com/nextmove/transaction_service/config/FeignClientConfig.java
+++ services/transaction-service/src/main/java/com/nextmove/transaction_service/config/FeignClientConfig.java
@@ -1,14 +1,15 @@
 package com.nextmove.transaction_service.config;
 
-import com.nextmove.transaction_service.util.GetEnv;
 import feign.RequestInterceptor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class FeignClientConfig {
 
-    private final String serviceToken = GetEnv.get("FIXED_SERVICE_TOKEN");
+    @Value("${fixed.service.token}")
+    private String serviceToken;
 
     @Bean
     public RequestInterceptor requestInterceptor() {

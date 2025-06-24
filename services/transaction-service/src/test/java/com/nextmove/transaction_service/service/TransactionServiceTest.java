4050e3070387bc270e067e4f6c7c5a0427aba797
eduardo-toste <duciochetti@gmail.com>
2025-06-24 19:02:07 -0300

    test: create get all transactions test

--- services/transaction-service/src/test/java/com/nextmove/transaction_service/service/TransactionServiceTest.java
+++ services/transaction-service/src/test/java/com/nextmove/transaction_service/service/TransactionServiceTest.java
@@ -14,17 +14,19 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 
+import static com.nextmove.transaction_service.util.BuilderUtil.buildTransactionRequest;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class TransactionServiceTest {
@@ -42,34 +44,56 @@ class TransactionServiceTest {
     private TransactionReminderProducer transactionReminderProducer;
 
     private UUID userId;
-    private TransactionRequestDTO request;
+    private TransactionRequestDTO salaryRequest;
 
     @BeforeEach
     void setup() {
         userId = UUID.randomUUID();
-        request = new TransactionRequestDTO(
-                "Salário",
-                "Salário do mês atual",
-                new BigDecimal("5000.0"),
-                LocalDate.of(2025, 7, 5),
-                TransactionType.INCOME
-        );
+        salaryRequest = buildTransactionRequest("Salário", "Salário do mês atual", "5000.0", TransactionType.INCOME);
     }
 
     @Test
     void shouldCreateTransactionSuccessfully() {
         // Arrange
-        Transaction transaction = TransactionMapper.toEntity(request, userId);
+        Transaction transaction = TransactionMapper.toEntity(salaryRequest, userId);
         when(transactionRepository.save(any(Transaction.class))).thenReturn(transaction);
 
         // Act
-        TransactionResponseDTO response = transactionService.createTransaction(userId, request);
+        TransactionResponseDTO response = transactionService.createTransaction(userId, salaryRequest);
 
         // Assert
         assertNotNull(response);
         assertEquals("Salário", response.title());
         assertEquals(new BigDecimal("5000.0"), response.amount());
         assertEquals("INCOME", response.type().name());
+
         verify(transactionRepository, times(1)).save(any(Transaction.class));
     }
+
+    @Test
+    void shouldReturnAllTransactionsFromUser() {
+        // Arrange
+        Pageable pageable = Pageable.ofSize(2).withPage(0);
+
+        TransactionRequestDTO internetRequest = buildTransactionRequest("Internet", "Plano mensal", "150", TransactionType.EXPENSE);
+        TransactionRequestDTO waterRequest = buildTransactionRequest("Água", "Conta de água", "80", TransactionType.EXPENSE);
+
+        Transaction internetTx = TransactionMapper.toEntity(internetRequest, userId);
+        Transaction aguaTx = TransactionMapper.toEntity(waterRequest, userId);
+
+        Page<Transaction> transactionPage = new PageImpl<>(List.of(internetTx, aguaTx), pageable, 2);
+        when(transactionRepository.findByUserId(userId, pageable)).thenReturn(transactionPage);
+
+        // Act
+        Page<TransactionResponseDTO> result = transactionService.getAllTransactions(userId, pageable);
+
+        // Assert
+        assertNotNull(result);
+        assertEquals(2, result.getContent().size());
+        assertEquals("Internet", result.getContent().get(0).title());
+        assertEquals("Água", result.getContent().get(1).title());
+
+        verify(transactionRepository, times(1)).findByUserId(userId, pageable);
+    }
+
 }

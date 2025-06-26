6bef85087160d59e376bb6cd5c1c78782306d571
eduardo-toste <duciochetti@gmail.com>
2025-06-25 22:06:11 -0300

    test: create user service test file

---
+++ services/auth-service/src/test/java/com/nextmove/auth_service/service/UserServiceTest.java
@@ -0,0 +1,26 @@
+package com.nextmove.auth_service.service;
+
+import com.nextmove.auth_service.producer.UserEventProducer;
+import com.nextmove.auth_service.repository.UserRepository;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+
+    @InjectMocks
+    private UserService userService;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    @Mock
+    private UserEventProducer userEventProducer;
+
+}

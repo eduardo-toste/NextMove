e973e2d209a7dabda9105dc459f8c2c1c1b2b597
eduardo-toste <duciochetti@gmail.com>
2025-06-29 18:02:13 -0300

    fix: update min value tag from password

--- services/auth-service/src/main/java/com/nextmove/auth_service/dto/RegisterRequestDTO.java
+++ services/auth-service/src/main/java/com/nextmove/auth_service/dto/RegisterRequestDTO.java
@@ -1,8 +1,8 @@
 package com.nextmove.auth_service.dto;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record RegisterRequestDTO(
 
@@ -14,6 +14,6 @@ public record RegisterRequestDTO(
         String username,
 
         @NotBlank
-        @Min(6)
+        @Size(min = 6)
         String password) {
 }

85b970dbbc1849b1f51d16ec2921cd74699575b3
eduardo-toste <duciochetti@gmail.com>
2025-06-29 17:31:51 -0300

    test: create authentication controller test file

---
+++ services/auth-service/src/test/java/com/nextmove/auth_service/controller/AuthenticationControllerTest.java
@@ -0,0 +1,10 @@
+package com.nextmove.auth_service.controller;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@WebMvcTest(AuthenticationController.class)
+class AuthenticationControllerTest {
+
+}
